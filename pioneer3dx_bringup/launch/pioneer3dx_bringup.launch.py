4f5a3ee6d57cc96d67c0ce0687f6a77625397ef7
David <davidnike80@gmail.com>
2025-10-24 10:01:28 -0300

    Corrigi que o bringup nao chamava o ydlidar

--- pioneer3dx_bringup/launch/pioneer3dx_bringup.launch.py
+++ pioneer3dx_bringup/launch/pioneer3dx_bringup.launch.py
@@ -2,6 +2,8 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
 
@@ -21,5 +23,10 @@ def generate_launch_description():
             namespace='pioneer3DX',
             output='screen',
         ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(lidar_launch)
+        ),
+
     ]
 )
